035e49e074a72410f33b8a861de1431ecb1fe890
Lineu Akiuki <linakiishi@gmail.com>
2025-08-04 21:43:10 -0300

    shopping e casting

---
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao03/aula05/Exemplo01.java
@@ -0,0 +1,21 @@
+package br.com.senaisp.bauru.secao03.aula05;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+		char letra = 65;
+		System.out.println(letra);
+		letra = 'A';
+		System.out.println(letra);
+		letra++; // passa para letra B
+		System.out.println(letra);
+		letra--; // volta para letra A
+		System.out.println(letra);
+		// cuidados ao imprimir mais de uma letra
+		// o exemplo abaixo soma os valores, não concatena (não junta as letras)
+		System.out.println(letra + letra);
+		// nesse outro exemplo iremos concatenar (junta as letras)
+		System.out.println("" + letra + letra);
+	}
+
+}
